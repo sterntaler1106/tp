f77c7e891f18aec26904946ee2f9165f68a9c96e
Esther Fuhrmann <esther.fuhrmann@arxes-tolina.de>
2018-12-27 13:04:39 +0100

    Beer-Tutorial-Projekt angelegt, Backendprojekt generiert

---
+++ bootifulWithReact/beer-backend/src/main/java/com/example/beerbackend/BeerBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.beerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerBackendApplication.class, args);
+	}
+
+}
+
