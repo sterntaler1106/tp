7574d975a0ce2c5110898fc6e0c2a99e63b74767
Esther Fuhrmann <esther.fuhrmann@arxes-tolina.de>
2019-01-07 15:33:50 +0100

    starting the burger component

--- udemy-burger-app/src/components/Burger/Burger.tsx
+++ udemy-burger-app/src/components/Burger/Burger.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
 
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import * as classes from './Burger.css'
+
 interface BurgerProps {
 
 }
 
 const burger = (props: BurgerProps) => {
-
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type='bread-top'></BurgerIngredient>
+            <BurgerIngredient type='cheese'></BurgerIngredient>
+            <BurgerIngredient type='meat'></BurgerIngredient>
+            <BurgerIngredient type='bread-bottom'></BurgerIngredient>
+        </div>
+    );
 };
 
 export default burger;
