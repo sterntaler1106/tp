a482ab0e09e67a0a96e46fe61f3411a4d6fd342f
Esther Fuhrmann <esther.fuhrmann@arxes-tolina.de>
2019-01-07 12:48:12 +0100

    started implementing burger builder

--- udemy-burger-app/src/components/Layout/Layout.tsx
+++ udemy-burger-app/src/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 
-import Aux from '../../hoc/Auxiliary'
+import Aux from '../../hoc/Auxiliary';
+import * as classes from './Layout.css';
 
 interface LayoutProps {
     children: React.ReactNode;
@@ -9,7 +10,7 @@ interface LayoutProps {
 const layout = (props: LayoutProps) => (
         <Aux>
             <div>Toolbar, SideDrawer, Backdrop</div>
-            < main>
+            < main className={classes.Content}>
                 {props.children}
             </main>
         </Aux>
