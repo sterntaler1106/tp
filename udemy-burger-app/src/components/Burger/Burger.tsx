8e838f7e6e38ed7cd42e60a4f1cd6464a608cee4
Esther Fuhrmann <esther.fuhrmann@arxes-tolina.de>
2019-01-07 16:25:14 +0100

    outputting burger ingredients

--- udemy-burger-app/src/components/Burger/Burger.tsx
+++ udemy-burger-app/src/components/Burger/Burger.tsx
@@ -4,15 +4,29 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import * as classes from './Burger.css'
 
 interface BurgerProps {
+    ingredients: Ingredients
+}
 
+// TODO: Duplication von Interface verhindern
+interface Ingredients {
+    salad: number;
+    bacon: number;
+    cheese: number;
+    meat: number;
 }
 
 const burger = (props: BurgerProps) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array((props.ingredients as any)[igKey])].map((_, i) => { // TODO index signature nochmal nachlesen
+                return <BurgerIngredient key={igKey + i} type={igKey}/>
+            })
+        });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top'></BurgerIngredient>
-            <BurgerIngredient type='cheese'></BurgerIngredient>
-            <BurgerIngredient type='meat'></BurgerIngredient>
+            {transformedIngredients}
             <BurgerIngredient type='bread-bottom'></BurgerIngredient>
         </div>
     );
--- udemy-burger-app/src/containers/BurgerBuilder/BurgerBuilder.tsx
+++ udemy-burger-app/src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -3,11 +3,36 @@ import * as React from "react";
 import Aux from '../../hoc/Auxiliary'
 import Burger from "../../components/Burger/Burger";
 
-class BurgerBuilder extends React.Component {
+interface BurgerBuilderProps {
+
+}
+
+interface BurgerBuilderState {
+    ingredients: Ingredients
+}
+
+// TODO: Duplication von Interface verhindern
+interface Ingredients {
+    salad: number;
+    bacon: number;
+    cheese: number;
+    meat: number;
+}
+
+class BurgerBuilder extends React.Component<BurgerBuilderProps, BurgerBuilderState> {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render() {
         return (
             <Aux>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aux>
         );
