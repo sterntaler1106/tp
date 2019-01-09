6f25406db0c588189e3b41b4dec840533d2a5105
Esther Fuhrmann <esther.fuhrmann@arxes-tolina.de>
2019-01-09 17:12:49 +0100

    creating a responsive sidedrawer

--- udemy-burger-app/src/components/Layout/Layout.tsx
+++ udemy-burger-app/src/components/Layout/Layout.tsx
@@ -4,17 +4,17 @@ import Aux from '../../hoc/Auxiliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import * as classes from './Layout.css';
 import {PropsWithChildren} from "../../interfaces/Interfaces";
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = (props: PropsWithChildren) => (
-        <Aux>
-            <Toolbar/>
-            < main className={classes.Content}>
-                {props.children}
-            </main>
-        </Aux>
-
-    )
-;
+    <Aux>
+        <Toolbar/>
+        <SideDrawer/>
+        < main className={classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
+);
 
 export default layout;
 
---
+++ udemy-burger-app/src/components/Navigation/SideDrawer/SideDrawer.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+
+import * as classes from './SideDrawer.css'
+
+interface SidedrawerProps {
+}
+
+const sideDrawer = (props: SidedrawerProps) => {
+
+    return (
+        <div className={classes.SideDrawer}>
+            <Logo/>
+            <nav>
+                <NavigationItems/>
+            </nav>
+        </div>
+    );
+};
+
+export default sideDrawer;
