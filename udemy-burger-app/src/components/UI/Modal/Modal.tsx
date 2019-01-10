a92596ba78c5092fa2d0bffc8b9c5c1901b7bf89
Esther Fuhrmann <esther.fuhrmann@arxes-tolina.de>
2019-01-10 12:06:01 +0100

    Improving Performance

--- udemy-burger-app/src/components/UI/Modal/Modal.tsx
+++ udemy-burger-app/src/components/UI/Modal/Modal.tsx
@@ -9,17 +9,24 @@ interface ModalProps extends PropsWithChildren {
     modalClosed: any; // TODO besseren Type wählen
 }
 
+class Modal extends React.Component<ModalProps, {}> {
+    shouldComponentUpdate(nextProps: Readonly<ModalProps>, nextState: Readonly<{}>, nextContext: any): boolean {
+        return nextProps.show != this.props.show;
+    }
 
-const modal = (props: ModalProps) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div className={classes.Modal} style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? 1 : 0
-        }}>
-            {props.children}
-        </div>
-    </Aux>
-);
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className={classes.Modal} style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? 1 : 0
+                }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+    }
+}
 
-export default modal;
+export default Modal;
