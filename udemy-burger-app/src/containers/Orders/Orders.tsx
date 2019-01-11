3cca0ad9ead96c129b387e9bcc6e17895271559f
Esther Fuhrmann <esther.fuhrmann@arxes-tolina.de>
2019-01-11 15:46:10 +0100

    fetching orders

--- udemy-burger-app/src/containers/Orders/Orders.tsx
+++ udemy-burger-app/src/containers/Orders/Orders.tsx
@@ -1,10 +1,38 @@
 import * as React from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
-interface OrdersProps {
+interface OrdersState {
+    orders: any; // TODO any nicht ideal
+    loading: boolean;
 }
 
-class Orders extends React.Component {
+class Orders extends React.Component<{}, OrdersState> {
+
+    state = {
+        orders: [],
+        loading: true,
+    }
+
+    componentDidMount(): void {
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+                }
+
+                this.setState({loading: false, orders: fetchedOrders})
+            })
+            .catch(error => {
+                this.setState({loading: false})
+            });
+    }
+
     render() {
         return (
             <div>
@@ -15,4 +43,4 @@ class Orders extends React.Component {
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
