7679c534053c919b142fb7dda7711154638e3297
Esther Fuhrmann <esther.fuhrmann@arxes-tolina.de>
2019-01-11 15:30:06 +0100

    implementing navigation links

--- udemy-burger-app/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.tsx
+++ udemy-burger-app/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.tsx
@@ -2,18 +2,20 @@ import * as React from 'react';
 
 import * as classes from './NavigationItem.css'
 import {PropsWithChildren} from "../../../../interfaces/Interfaces";
+import {NavLink} from "react-router-dom";
 
 interface NavigationItemProps extends PropsWithChildren {
     link: string;
-    active: boolean;
+    exact: boolean;
 }
 
 const navigationItem = (props: NavigationItemProps) => (
     <li className={classes.NavigationItem}>
-        <a
-            href={props.link}
-            className={props.active ? classes.active : ''}
-        >{props.children}</a>
+        <NavLink
+            to={props.link}
+            exact={props.exact}
+            activeClassName={classes.active}
+        >{props.children}</NavLink>
     </li>
 );
 
--- udemy-burger-app/src/components/Navigation/NavigationItems/NavigationItems.tsx
+++ udemy-burger-app/src/components/Navigation/NavigationItems/NavigationItems.tsx
@@ -8,8 +8,8 @@ interface NavigationItemsProps {
 
 const navigationItems = (props: NavigationItemsProps) => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link="/" active={true}>Burger Builder</NavigationItem>
-        <NavigationItem link="/" active={false}>Checkout</NavigationItem>
+        <NavigationItem link="/" exact={true}>Burger Builder</NavigationItem>
+        <NavigationItem link="/orders" exact={false}>Orders</NavigationItem>
     </ul>
 );
 
